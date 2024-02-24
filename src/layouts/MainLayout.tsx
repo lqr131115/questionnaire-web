3b49b915fd47227578405799ed5600659f4df898
lqr131115 <1046975211@qq.com>
2024-02-24 21:32:30 +0800

    refactor: remove components

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -2,8 +2,8 @@ import React, { FC } from "react";
 import { Outlet } from "react-router-dom";
 import { Layout } from "antd";
 import styles from "./MainLayout.module.scss";
-import Logo from "./Logo";
-import UserInfo from "./UserInfo";
+import Logo from "../components/Logo";
+import UserInfo from "../components/UserInfo";
 const { Header, Footer, Content } = Layout;
 
 const MainLayout: FC = () => {
