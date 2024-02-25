65f95cfd8fd478d49f8a5031d905400efda3dc5e
lqr131115 <1046975211@qq.com>
2024-02-25 15:16:19 +0800

    feat: 404 page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,20 @@
 import React, { FC } from "react";
-
+import { Button, Result } from "antd";
+import { useNavigate } from "react-router-dom";
 const NotFound: FC = () => {
-  return <div>I AM 404</div>;
+  const nav = useNavigate();
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry, the page you visited does not exist."
+      extra={
+        <Button type="link" onClick={() => nav(-1)}>
+          Back
+        </Button>
+      }
+    />
+  );
 };
 
 export default NotFound;
