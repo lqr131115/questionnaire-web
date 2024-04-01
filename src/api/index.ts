80a66df5c60789ce6d946519b1126e7a63b213f2
lqr131115 <1046975211@qq.com>
2024-04-01 11:17:50 +0800

    feat: qn detail api

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,7 +2,15 @@ import request from "../utils/request";
 import { RequestEnum } from "../constants/enum";
 
 export const QUESTIONNAIRE_LIST_URL = "/question/list";
+export const QUESTIONNAIRE_DETAIL_URL = "/question/:id";
 // const SEARCH_STAR_LIST_URL = '/star/list';
 
 export const getQNList = () =>
   request({ method: RequestEnum.GET, url: QUESTIONNAIRE_LIST_URL });
+
+export const getQNDetail = (id: string) =>
+  request({
+    method: RequestEnum.GET,
+    url: QUESTIONNAIRE_DETAIL_URL,
+    params: { id },
+  });
--- src/pages/question/Edit/index.tsx
+++ src/pages/question/Edit/index.tsx
@@ -1,10 +1,22 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useTitle } from "ahooks";
+import { getQNDetail } from "../../../api";
 const Edit: FC = () => {
   useTitle("问卷编辑");
   const params = useParams();
-  return <div>I AM {JSON.stringify(params)}</div>;
+  const [detail, setDetail] = useState<any>({});
+  useEffect(() => {
+    getQNDetail(params.id as string).then((res) => {
+      setDetail(res.data);
+    });
+  }, []);
+  return (
+    <>
+      I AM {JSON.stringify(params)}
+      <h1>{detail.title}</h1>
+    </>
+  );
 };
 
 export default Edit;
