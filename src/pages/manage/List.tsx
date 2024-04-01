76ac9abab49ef1188e65e4ab00f2b6e1a98321e2
lqr131115 <1046975211@qq.com>
2024-04-01 21:35:06 +0800

    feat: list load more

--- src/pages/manage/List.tsx
+++ src/pages/manage/List.tsx
@@ -1,11 +1,16 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { useTitle } from "ahooks";
-import { Empty, Spin } from "antd";
+import { Empty, Flex, Spin, Button } from "antd";
+import { useLocation, useNavigate } from "react-router-dom";
 import type { Questionnaire } from "./manage";
 import styles from "./List.module.scss";
 import { useQNList } from "../../hooks";
 import QuestionCard from "../../components/QuestionCard";
 import QuestionHeader from "../../components/QuestionHeader";
+import {
+  SEARCH_LIST_DEFAULT_PAGESIZE,
+  SEARCH_LIST_PAGESIZE_KEY,
+} from "../../constants";
 
 const List: FC = () => {
   useTitle("我的问卷");
@@ -19,25 +24,51 @@ const List: FC = () => {
     alert(`删除问卷${id}`);
   }
 
+  const [pageSize, setPageSize] = useState(SEARCH_LIST_DEFAULT_PAGESIZE);
   const { loading, data: resData } = useQNList();
   const { data: questionList, total } = (resData || {}) as any;
+  const navigator = useNavigate();
+  const { pathname } = useLocation();
+  const noMore = pageSize >= total;
+  function handleLoadMore() {
+    const curSize = pageSize + SEARCH_LIST_DEFAULT_PAGESIZE;
+    navigator({
+      pathname,
+      search: `${SEARCH_LIST_PAGESIZE_KEY}=${curSize}`,
+    });
+    setPageSize(curSize);
+  }
+
   return (
     <>
       <QuestionHeader title="我的问卷" />
       <div className={styles.container}>
-        <h1>{total}</h1>
         <Spin spinning={loading} size="large">
           {questionList?.length === 0 && <Empty />}
-          {questionList?.length > 0 &&
-            questionList.map((item: Questionnaire) => (
-              <QuestionCard
-                key={item.id}
-                {...item}
-                copy={doCopy}
-                del={doDelete}
-                star={doStar}
-              />
-            ))}
+          {questionList?.length > 0 && (
+            <>
+              {questionList.map((item: Questionnaire) => (
+                <QuestionCard
+                  key={item.id}
+                  {...item}
+                  copy={doCopy}
+                  del={doDelete}
+                  star={doStar}
+                />
+              ))}
+              <Flex justify="center">
+                <Button
+                  type="link"
+                  block
+                  size="large"
+                  onClick={handleLoadMore}
+                  disabled={noMore}
+                >
+                  {noMore ? "No More" : "Load more"}
+                </Button>
+              </Flex>
+            </>
+          )}
         </Spin>
       </div>
     </>
