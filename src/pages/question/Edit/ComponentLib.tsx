b42680eec10ba32b84c6ac59e840410087f89319
lqr131115 <1046975211@qq.com>
2024-04-08 15:01:37 +0800

    style: use grid layout

--- src/pages/question/Edit/ComponentLib.tsx
+++ src/pages/question/Edit/ComponentLib.tsx
@@ -1,13 +1,14 @@
-import React, { FC } from "react";
-import { Typography, Card, Flex } from "antd";
+import React, { FC, useState } from "react";
+import { Typography, Row, Col, Flex } from "antd";
 import Icon from "@ant-design/icons/lib/components/Icon";
 import { qncMaterialGroup } from "@/components/QNComponents";
 import styles from "./ComponentLib.module.scss";
 
 const { Title } = Typography;
-const { Grid } = Card;
+const borderStyle = "1px solid #f0f0f0";
 
 const ComponentLib: FC = () => {
+  const [cols] = useState(3);
   return (
     <div className={styles.lib}>
       {qncMaterialGroup.map((group, index) => {
@@ -17,19 +18,27 @@ const ComponentLib: FC = () => {
             <Title level={4} style={{ marginTop: index === 0 ? 0 : 10 }}>
               {groupName}
             </Title>
-            <Card bordered={false} className={styles.card}>
+            <Row className={styles.row}>
               {components.map((c, i) => {
                 const { title, type, icon: IconSvg } = c;
                 return (
-                  <Grid key={type + i} className={styles.grid}>
+                  <Col
+                    key={type + i}
+                    className={styles.col}
+                    span={24 / cols}
+                    style={{
+                      borderTop: i < cols ? borderStyle : "none",
+                      borderLeft: i % cols === 0 ? borderStyle : "none",
+                    }}
+                  >
                     <Flex justify="center" align="center">
                       {<Icon component={() => <IconSvg />} />}
                       <span className={styles.title}> {title}</span>
                     </Flex>
-                  </Grid>
+                  </Col>
                 );
               })}
-            </Card>
+            </Row>
           </div>
         );
       })}
