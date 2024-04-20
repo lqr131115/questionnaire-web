396ad746b7de02cf6325404f262faba0a78f2c49
lqr131115 <1046975211@qq.com>
2024-04-20 15:32:17 +0800

    feat: antv

--- src/pages/question/Stat/ChartStat.tsx
+++ src/pages/question/Stat/ChartStat.tsx
@@ -1,11 +1,30 @@
 import React, { FC } from "react";
 import { Typography } from "antd";
+import { Line } from "@ant-design/charts";
+import type { LineConfig } from "@ant-design/charts";
 
 const { Title } = Typography;
 
 type ChartStatProps = {
   type: string;
 };
+const LineChart: FC = () => {
+  const data = [
+    { year: "1996", value: 6 },
+    { year: "1997", value: 7 },
+    { year: "1998", value: 9 },
+    { year: "1999", value: 13 },
+  ];
+
+  const config: LineConfig = {
+    data,
+    height: 400,
+    xField: "year",
+    yField: "value",
+  };
+  return <Line {...config} />;
+};
+
 const ChartStat: FC<ChartStatProps> = (props) => {
   const { type } = props;
   return (
@@ -13,6 +32,7 @@ const ChartStat: FC<ChartStatProps> = (props) => {
       <Title level={3} style={{ margin: "5px 0" }}>
         统计 {type}
       </Title>
+      <LineChart />
     </>
   );
 };
